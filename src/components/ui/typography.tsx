fcf53d4934bd31d34840a53a177b411f2872f925
Mattias Wiberg <mattiaswibb@gmail.com>
2024-04-01 14:02:14 +0200

    change: added possibility to add classnames to typography functions

--- src/components/ui/typography.tsx
+++ src/components/ui/typography.tsx
@@ -1,90 +1,85 @@
-export function TypographyH1({ children }: { children: React.ReactNode }) {
+export function TypographyH1({ children, className }: { children: React.ReactNode, className?: string}) {
     return (
-      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+      <h1 className={`scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl ${className}`}>
         { children }
       </h1>
     )
   }
 
-export function TypographyH2({ children }: { children: React.ReactNode }) {
+export function TypographyH2({ children, className }: { children: React.ReactNode, className?: string}) {
   return (
-    <h2 className="mt-10 scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
+    <h2 className={`scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0 ${className}`}>
       { children }
     </h2>
   )
 }
 
-export function TypographyH3({ children }: { children: React.ReactNode }) {
+export function TypographyH3({ children, className }: { children: React.ReactNode, className?: string}) {
   return (
-    <h3 className="mt-8 scroll-m-20 text-2xl font-semibold tracking-tight first:mt-0">
+    <h3 className={`scroll-m-20 text-2xl font-semibold tracking-tight first:mt-0 ${className}`}>
       { children }
     </h3>
   )
 }
 
-export function TypographyH4({ children }: { children: React.ReactNode }) {
+export function TypographyH4({ children, className }: { children: React.ReactNode, className?: string}) {
   return (
-    <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
+    <h4 className={`scroll-m-20 text-xl font-semibold tracking-tight ${className}`}>
       { children }
     </h4>
   )
 }
 
-export function TypographyP({ children }: { children: React.ReactNode }) {
+export function TypographyP({ children, className }: { children: React.ReactNode, className?: string}) {
   return (
-    <p className="leading-7 [&:not(:first-child)]:mt-6">
+    <p className={`leading-7 [&:not(:first-child)]:mt-6 ${className}`}>
       { children }
     </p>
   )
 }
 
-export function TypographyBlockquote({ children }: { children: React.ReactNode }) {
+export function TypographyBlockquote({ children, className }: { children: React.ReactNode, className?: string}) {
   return (
-    <blockquote className="mt-6 border-l-2 pl-6 italic">
+    <blockquote className={`mt-6 border-l-2 pl-6 italic ${className}`}>
       { children }
     </blockquote>
   )
 }
 
-export function TypographyInlineCode({ children }: { children: React.ReactNode }) {
+export function TypographyInlineCode({ children, className }: { children: React.ReactNode, className?: string}) {
   return (
-    <code className="relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold">
+    <code className={`relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold ${className}`}>
       { children }
     </code>
   )
 }
 
-export function TypographyLead({ children }: { children: React.ReactNode }) {
+export function TypographyLead({ children, className }: { children: React.ReactNode, className?: string}) {
   return (
-    <p className="text-xl text-muted-foreground">
+    <p className={`text-xl text-muted-foreground ${className}`}>
       { children }
     </p>
   )
 }
 
-export function TypographyLarge({ children }: { children: React.ReactNode }) {
-  return <div className="text-lg font-semibold">
+export function TypographyLarge({ children, className }: { children: React.ReactNode, className?: string}) {
+  return <div className={`text-lg font-semibold ${className}`}>
     { children }
     </div>
 }
 
-export function TypographySmall({ children }: { children: React.ReactNode }) {
+export function TypographySmall({ children, className }: { children: React.ReactNode, className?: string}) {
   return (
-    <small className="text-sm font-medium leading-none">
+    <small className={`text-sm font-medium leading-none ${className}`}>
       { children }
       </small>
   )
 }
 
-export function TypographyMuted({ children }: { children: React.ReactNode }) {
+export function TypographyMuted({ children, className }: { children: React.ReactNode, className?: string}) {
   return (
-    <p className="text-sm text-muted-foreground">
+    <p className={`text-sm text-muted-foreground ${className}`}>
       { children }
       </p>
   )
 }
-
-
-
-
-
