f9c1dacaf9c33773ffb57aa21e07e76521ae097b
Mattias Wiberg <mattiaswibb@gmail.com>
2024-04-05 18:25:24 +0200

    feat: added loading page

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,6 +1,7 @@
 import { auth } from "@/firebase";
 import { User, onAuthStateChanged } from "firebase/auth";
 import { ReactNode, createContext, useContext, useEffect, useState } from "react";
+import piggyTheBuilder from "@/assets/piggy-the-builder.png";
 
 const AuthContext = createContext<User | null>(null);
 
@@ -10,8 +11,14 @@ export function useAuth() {
 
 export function AuthProvider({ children }: { children: ReactNode }) {
     const [user, setUser] = useState<User | null>(null);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
+
+        auth.authStateReady().then(() => {
+            setLoading(false);
+        })
+
         const unsub = onAuthStateChanged(auth, (user: User | null) => {
             setUser(user)
         })
@@ -23,9 +30,22 @@ export function AuthProvider({ children }: { children: ReactNode }) {
 
     return (
         <>
-            <AuthContext.Provider value={user}>
-                {children}
-            </AuthContext.Provider>
+            {
+                loading ? (
+                    <div className="h-screen flex justify-center items-center">
+                        <div className="flex flex-col justify-center items-center">
+                            <img src={piggyTheBuilder} alt="logo" className="w-1/2 h-1/2 animate-pulse" />
+                            <div className="text-3xl font-bold mt-10">The piggy is building!</div>
+                            <div className="text-[0.70rem] text-muted-foreground mt-1">
+                                The website is currently under construction. Please wait for a while.
+                            </div>
+                        </div>
+                    </div >
+                ) :
+                    <AuthContext.Provider value={user}>
+                        {children}
+                    </AuthContext.Provider>
+            }
         </>
     );
 }
