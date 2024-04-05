9704efcf68356df192c3da087e746a879c356a26
Mattias Wiberg <mattiaswibb@gmail.com>
2024-04-05 11:16:53 +0200

    fix: protected home route

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,4 +1,15 @@
+import { useAuth } from "@/context/AuthContext"
+import { useEffect } from "react"
+import { useNavigate } from "react-router-dom"
+
 function Home() {
+    const user = useAuth()
+    const navigate = useNavigate()
+    useEffect(() => {
+        if (!user) {
+            navigate('/auth')
+        }
+    }, [user])
     return (
         <div>Home</div>
     )
