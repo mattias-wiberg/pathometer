d3abfbf38c8f49628ba4e5e89f7ea4c217ec646f
Mattias Wiberg <mattiaswibb@gmail.com>
2024-04-05 11:16:36 +0200

    feat: reworked navbar and added user nav

---
+++ src/components/ui/user-nav.tsx
@@ -0,0 +1,75 @@
+import {
+    Avatar,
+    AvatarFallback,
+    AvatarImage,
+} from "@/components/ui/avatar"
+import { Button } from "@/components/ui/button"
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuGroup,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { useAuth } from "@/context/AuthContext"
+import { auth } from "@/firebase"
+import { Auth, signOut } from "firebase/auth"
+import { useNavigate } from "react-router-dom"
+
+export function UserNav() {
+    const user = useAuth()
+    const navigate = useNavigate()
+    const signOutUser = (auth: Auth) => {
+        signOut(auth).then(() => {
+            navigate('/auth')
+        }).catch((error) => {
+            console.error('signOutUser', error)
+        })
+    }
+
+    function getInitials(fullName: string): string {
+        const [firstName, lastName] = fullName.split(" ");
+        const firstInitial = firstName.charAt(0).toUpperCase();
+        const lastInitial = lastName.charAt(0).toUpperCase();
+        return `${firstInitial}${lastInitial}`;
+    }
+
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <Button variant="ghost" className="relative h-8 w-8 rounded-full">
+                    <Avatar className="h-8 w-8">
+                        <AvatarImage src={user?.photoURL ?? ""} alt="@shadcn" />
+                        <AvatarFallback>{user?.displayName && getInitials(user.displayName)}</AvatarFallback>
+                    </Avatar>
+                </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-56" align="end" forceMount>
+                <DropdownMenuLabel className="font-normal">
+                    <div className="flex flex-col space-y-1">
+                        <p className="text-sm font-medium leading-none">{user?.displayName}</p>
+                        <p className="text-xs leading-none text-muted-foreground">
+                            {user?.email}
+                        </p>
+                    </div>
+                </DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuGroup>
+                    <DropdownMenuItem>
+                        Profile
+                    </DropdownMenuItem>
+                    <DropdownMenuItem>
+                        Settings
+                    </DropdownMenuItem>
+                    <DropdownMenuItem>New Team</DropdownMenuItem>
+                </DropdownMenuGroup>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem onClick={() => signOutUser(auth)}>
+                    Log out
+                </DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    )
+}
--- src/pages/components/navbar.tsx
+++ src/pages/components/navbar.tsx
@@ -1,38 +1,35 @@
 import { Button } from '@/components/ui/button'
 import { ModeToggle } from '@/components/ui/mode-toggle'
-import { TypographyH3, TypographyP } from '@/components/ui/typography'
+import { TypographyH3 } from '@/components/ui/typography'
 import { useAuth } from '@/context/AuthContext'
-import { auth } from '@/firebase'
-import { signOut } from 'firebase/auth'
 import { MdModeOfTravel } from 'react-icons/md'
-import { NavLink, useNavigate } from 'react-router-dom'
-import { IoEnterOutline, IoExitOutline } from "react-icons/io5";
+import { NavLink } from 'react-router-dom'
+import { NotebookText } from 'lucide-react'
+import { UserNav } from '@/components/ui/user-nav'
 
 const Navbar = () => {
-  const navigate = useNavigate()
   const user = useAuth()
 
   return (
     <div className="border-b">
       <div className="flex h-16 items-center px-4">
-        <NavLink to='/' className='flex items-center space-x-2'>
+        <NavLink to='/' className='flex items-center'>
           <MdModeOfTravel size={32} />
           <TypographyH3 className='ml-2'>Work Travel Tracker</TypographyH3>
+          <p className={`mt-6 text-xs`}>
+            {import.meta.env.VITE_VERSION}
+          </p>
         </NavLink>
-        <p className={`mt-6 ml-0 text-xs`}>
-          {import.meta.env.VITE_VERSION}
-        </p>
 
         <div className="ml-auto flex items-center space-x-4">
           {
-            user ? (
+            user && (
               <>
-                <TypographyP>{user.email}</TypographyP>
-                <Button onClick={() => signOut(auth)}>Exit<IoExitOutline className='ml-2' /></Button>
-              </>
-            ) : (
-              <>
-                <Button onClick={() => navigate('/auth')}>Enter<IoEnterOutline className='ml-2' /></Button>
+                <Button variant="outline" size="icon">
+                  <NotebookText className="absolute h-[1.2rem] w-[1.2rem]" />
+                  <span className="sr-only">Add Trip</span>
+                </Button>
+                <UserNav />
               </>
             )
           }
