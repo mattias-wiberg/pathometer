9198ebccf4b76dd9f26a6384efc67838891a5e30
Mattias Wiberg <mattiaswibb@gmail.com>
2024-04-07 20:28:15 +0200

    feat: added landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import piggyTheBuilder from "@/assets/piggy-the-builder.webp";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="h-screen flex justify-center items-center">
+      <div className="flex flex-col justify-center items-center">
+        <img src={piggyTheBuilder} alt="Building Piggy" className="w-[300px] animate-pulse" />
+        <div className="text-3xl font-bold mt-10">The piggy is building!</div>
+        <div className="text-[0.70rem] text-muted-foreground mt-1">
+          The website is currently under construction. Please wait for a while.
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div >
   )
 }
 
