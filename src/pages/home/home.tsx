fdff55f1485fd8f57c1398a51d986066b12a1ce9
Mattias Wiberg <klutten99@gmail.com>
2024-04-13 13:00:21 +0200

    fix: added missing dependency to effect

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -15,7 +15,7 @@ function Home() {
         if (!user) {
             navigate('/auth')
         }
-    }, [user])
+    }, [user, navigate])
     return (
         <div className="flex-1 space-y-4 p-8 pt-6">
             <div className="flex items-center justify-between space-y-2 flex flex-col sm:flex-row">
