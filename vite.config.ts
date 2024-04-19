c3ab362c35243a35cff45b1e33bb20ee0f83a324
Mattias Wiberg <klutten99@gmail.com>
2024-04-19 22:25:46 +0200

    chore: added dev env and switched to process.env instead of vite for security.

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,24 @@
-import path from "path"
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path";
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    }
-  }
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+    define: {
+      "process.env.FIREBASE_API_KEY": `"${env.FIREBASE_API_KEY}"`,
+      "process.env.FIREBASE_AUTH_DOMAIN": `"${env.FIREBASE_AUTH_DOMAIN}"`,
+      "process.env.FIREBASE_PROJECT_ID": `"${env.FIREBASE_PROJECT_ID}"`,
+      "process.env.FIREBASE_STORAGE_BUCKET": `"${env.FIREBASE_STORAGE_BUCKET}"`,
+      "process.env.FIREBASE_MESSAGING_SENDER_ID": `"${env.FIREBASE_MESSAGING_SENDER_ID}"`,
+      "process.env.FIREBASE_APP_ID": `"${env.FIREBASE_APP_ID}"`,
+    },
+  };
+});
